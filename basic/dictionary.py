4407bbd2176f326e7dd36974192e356aa11c7481
salman <salmanmdsultan@gmail.com>
2023-11-21 22:37:37 +0600

    dictionary files added

---
+++ basic/dictionary.py
@@ -0,0 +1,9 @@
+cars = {"car1":
+        {"model":'m1',"price":100},
+        "car2":
+        {"model":'m2',"price":1000}}
+for i,j in cars.items():
+    for k in j.items():
+        print(i,k)
+
+#{key:value}
